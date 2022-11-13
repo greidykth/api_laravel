f68a761e7a777daae8e9df796065f47a1e08af18
Greidy Peña <greidykp@gmail.com>
2022-11-12 20:34:01 -0500

    Add event endpoint to create an account with an initial balance

--- database/migrations/2022_11_13_000246_create_accounts_table.php
+++ database/migrations/2022_11_13_000246_create_accounts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
-            $table->integer('balance');
+            $table->integer('balance')->default(0);
             $table->timestamps();
         });
     }
