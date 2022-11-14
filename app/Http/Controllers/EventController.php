605694472c775c30066b7e3b09ad5ff61a4ebd39
Greidy Peña <greidykp@gmail.com>
2022-11-14 11:54:34 -0500

    Add transfer event (from existing and non existing origin accounts)

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Account;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EventController extends Controller
 {
@@ -18,6 +19,12 @@ class EventController extends Controller
             return $this->withdraw(
                 $request->origin,
                 $request->amount
+            );
+        } elseif ($request->type === 'transfer') {
+            return $this->transfer(
+                $request->origin,
+                $request->amount,
+                $request->destination
             );
         }
     }
@@ -52,5 +59,32 @@ class EventController extends Controller
                 'balance' => $account->balance
             ]
         ], 200);
+    }
+
+    private function transfer($origin, $amount, $destination)
+    {
+        $accountOrigin = Account::findOrFail($origin);
+        $accountDestination = Account::firstOrCreate([
+            'id' => $destination
+        ]);
+
+        DB::transaction( function () use ($accountOrigin, $accountDestination, $amount) {
+            $accountOrigin->balance -= $amount;
+            $accountDestination->balance += $amount;
+
+            $accountOrigin->save();
+            $accountDestination->save();
+        });
+
+        return response()->json([
+            'origin' => [
+                'id' => $accountOrigin->id,
+                'balance' => $accountOrigin->balance
+            ],
+            'destination' => [
+                'id' => $accountDestination->id,
+                'balance' => $accountDestination->balance
+            ],
+        ], 201);
     }
 }
